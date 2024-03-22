a4644206654d8bf954cf3a296f7dc2386e317f63
spbovarnick <spbovarnick@gmail.com>
2024-03-21 17:54:35 -0700

    wip: execute script on all tabs

--- scripts/background.js
+++ scripts/background.js
@@ -1,30 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
+chrome.runtime.onStartup.addListener(() => {
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      chrome.scripting
+        .executeScript({
+          files: ["./scripts/blindrs-on.js"],
+          target: { tabId: tab.id },
+        });
+    });
   });
 });
-
-chrome.action.onClicked.addListener(async (tab) => {
-  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-  // Next state will always be the opposite
-  const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-  // Set the action badge to the next state
-  await chrome.action.setBadgeText({
-    tabId: tab.id,
-    text: nextState,
-  });
-
-  if (nextState === "ON") {
-    await chrome.scripting.insertCSS({
-      files: ["blindrs-on.css"],
-      target: { tabId: tab.id },
-    });
-  } else if (nextState === "OFF") {
-    await chrome.scripting.removeCSS({
-      files: ["blindrs-on.css"],
-      target: { tabId: tab.id },
-    });
-  }
-})
---
+++ scripts/blindrs-on.js
@@ -0,0 +1,6 @@
+console.log('fart')
+if (!document.getElementById('blindr')) {
+  const blindr = document.createElement('div');
+  blindr.id = 'blindr';
+  document.body.appendChild(blindr);
+}
